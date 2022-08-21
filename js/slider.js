4fae92e617778eff240966e0f907f120426909f2
Nexuslolz <a.klyosov@inbox.ru>
2022-08-21 06:54:11 +0500

    feat: complete slider

--- js/slider.js
+++ js/slider.js
@@ -10,12 +10,28 @@ $(document).ready(function () {
         asNavFor: '.slider-big',
         autoplaySpeed: 7000,
         pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    initialSlide: 0,
+                    slidesToShow: 1,
+                    centerMode: false,
+                    adaptiveHeight: false,
+                }
+            }
+        ]
     });
     $('.slider-big').slick({
         fade: true,
         arrows: false,
         asNavFor: '.slider',
         autoplaySpeed: 7000,
-
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: "unslick"
+            }
+        ]
     })
 })
